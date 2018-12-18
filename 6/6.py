3a40108949e791f3831a823b4c9f8ce5c994a698
martinjohansson93 <dat13mjo@student.lu.se>
2018-12-18 17:18:38 +0100

    Day6 done

--- 6/6.py
+++ 6/6.py
@@ -9,10 +9,8 @@ def find_most_reccuring(matrix, matrix_size, excluded):
 				sum_count_dict[key] = sum_count_dict[key] + value
 			except:
 				sum_count_dict[key] = value
-	print(excluded)
 	for v in excluded:
 		sum_count_dict.pop(str(int(v)), None)
-	print(sum_count_dict)
 	return max(sum_count_dict.values())
 
 def closest(matrix_cordinates, x, y):
@@ -35,11 +33,15 @@ def find_infity_values(matrix, matrix_size):
 		values_found.append(matrix[i][0])
 		values_found.append(matrix[matrix_size-1][i])
 		values_found.append(matrix[i][matrix_size-1])
-	print(set(values_found))
 	return set(values_found)
 
-def sol_1(input_list):
-	matrix = []
+def calc_sum_distance(matrix_cordinates, matrix_size, x, y):
+	sum_dis = 0
+	for cord in matrix_cordinates.values():
+		sum_dis += abs(cord['x'] - x) + abs(cord['y'] - y)
+	return True if sum_dis <= 10000 else False
+
+def parse_input(input_list):
 	x_values = []
 	y_values = []
 	index = 0
@@ -52,29 +54,30 @@ def sol_1(input_list):
 		matrix_dict[str(index)] = {'x': x, 'y': y}
 		index += 1
 	matrix_size = (max(max(x_values), max(y_values))) +1
+	return matrix_dict, matrix_size
+	
+def sol_1(input_list):
+	matrix = []
+	matrix_dict, matrix_size = parse_input(input_list)
 	for i in range(matrix_size):
 		matrix.append([])
 		for j in range(matrix_size):
 			matrix[i].append(closest(matrix_dict, i, j))
 	return find_most_reccuring(matrix, matrix_size, find_infity_values(matrix, matrix_size))
 
-def sol_2(string):
-	chars = []
-	for i in range(26):
-		chars.append(chr(ord('A') + i))
-	values = []
-	for a in chars:
-		new_string = string.replace(a, "").replace(a.lower(), "")
-		print(len(new_string))
-		print(len(string))
-		values.append(sol_1(new_string))
-	print(values)
-	return min(values)
+def sol_2(input_list):
+	matrix_dict, matrix_size = parse_input(input_list)
+	nbr_of_areas = 0
+	for i in range(matrix_size):
+		for j in range(matrix_size):
+			if calc_sum_distance(matrix_dict, matrix_size, i, j):
+				nbr_of_areas += 1
+	return nbr_of_areas
 
 with open("input.txt", 'r') as input_file:
     input_list = [x for x in input_file.readlines()]
 res1 = sol_1(input_list)
-# res2 = sol_2(input_list)
+res2 = sol_2(input_list)
 
 print("Result1: " + str(res1))
-# print("Result2: " + str(res2))
+print("Result2: " + str(res2))
