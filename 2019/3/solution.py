02666bb521ce0d73c20829df0a6e2945adf32536
Martin Johansson <martin.johansson@klarna.com>
2019-12-03 12:13:30 +0100

    day 3 done, not that nice though

--- 2019/3/solution.py
+++ 2019/3/solution.py
@@ -1,24 +1,66 @@
+from collections import Counter
+
 def parseInput():
     with open("input", 'r') as input_file:
-        return input_file.readlines()
+        lines = input_file.readlines()
+        wire1 = lines[0].split(',')
+        wire2 = lines[1].split(',')
+        return wire1, wire2
 
 def algorithm():
-    pass
-
-def one():
-    input_list = parseInput()
+    wire1, wire2 = parseInput()
     result = 0
-    for i in input_list:
-        algorithm()
-    print(1, result)
+    path1 = []
+    path2 = []
 
-def two():
-    input_list = parseInput()
-    result = 0
-    for i in input_list:
-        algorithm()
+    x = 0
+    y = 0
+    for wire in wire1:
+        dir = wire[:1]
+        for i in range(0, int(wire[1:])):
+            if dir == 'U':
+                y += 1
+            elif dir == 'D':
+                y -= 1
+            elif dir == 'R':
+                x += 1
+            elif dir == 'L':
+                x -= 1
+            path1.append((x, y))
+    x = 0
+    y = 0
+    for wire in wire2:
+        dir = wire[:1]
+        for i in range(0, int(wire[1:])):
+            if dir == 'U':
+                y += 1
+            elif dir == 'D':
+                y -= 1
+            elif dir == 'R':
+                x += 1
+            elif dir == 'L':
+                x -= 1
+            path2.append((x, y))
+    res = set(path1).intersection(path2)
+    res2 = list(res)
+    if res:
+        print(1, min([manhattan(i) for i in res]))
+    smallest_distance = 1000000000
+    for r in res:
+        if path2.index(r) + path1.index(r) < smallest_distance:
+            smallest_distance = path2.index(r) + path1.index(r)
 
-    print(2, result)
+    print(2, smallest_distance + 2 )
+
+def manhattan(res):
+    x = res[0]
+    y = res[1]
+    if x < 0:
+        x = x *-1
+    if y < 0:
+        y = y *-1
+    
+    return x + y
+
 if __name__ == "__main__":
-    one()
-    two()
+    algorithm()
