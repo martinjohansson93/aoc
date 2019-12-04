113da5d3d99863e025ead0e1b183dba1248a2b3a
Martin Johansson <martin.johansson@klarna.com>
2019-12-04 20:22:31 +0100

    second day done

--- 2019/4/solution.py
+++ 2019/4/solution.py
@@ -1,24 +1,56 @@
 def parseInput():
     with open("input", 'r') as input_file:
-        return input_file.readlines()
+        return "172851-675869"
 
 def algorithm():
     pass
 
-def one():
-    input_list = parseInput()
-    result = 0
-    for i in input_list:
-        algorithm()
-    print(1, result)
+def isTriple(value, i):
+    if i > 1:
+        if value[i-1] == value[i] and value[i-2] == value[i]:
+            return True
+    if i > 0 and i < len(value)-1:
+        if value[i-1] == value[i] and value[i+1] == value[i]:
+            return True
+    if i < len(value)-2:
+        if value[i+1] == value[i] and value[i+2] == value[i]:
+            return True
+    return False
 
-def two():
-    input_list = parseInput()
-    result = 0
-    for i in input_list:
-        algorithm()
+def isDouble(value, secondSolution=False):
+    for i in range(1, len(value)):
+        if i > 0:
+            if value[i-1] == value[i]:
+                if isTriple(value, i) and secondSolution:
+                    continue
+                return True
+    return False
 
-    print(2, result)
+def isIncreasing(value):
+    prev = 0
+    for i in range(0, len(value)):
+        if prev > value[i]:
+            return False
+        prev = value[i]
+    return True
+
+def solution():
+    res1 = []
+    res2 = []
+    for i in range(172851, 675870):
+        success = isIncreasing(str(i))
+        if not success:
+            continue
+        success = isDouble(str(i))
+        if not success:
+            continue
+        res1.append(i)
+        success = isDouble(str(i), secondSolution=True)
+        if not success:
+            continue
+        res2.append(i)
+    print(1, len(res1))
+    print(2, len(res2))
+    
 if __name__ == "__main__":
-    one()
-    two()
+    solution()
