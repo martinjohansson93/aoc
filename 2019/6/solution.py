d8080cfabd02842e5c0824a34eb33d825820ead3
Martin Johansson <martin.johansson@klarna.com>
2019-12-07 17:16:40 +0100

    day 6 done

--- 2019/6/solution.py
+++ 2019/6/solution.py
@@ -2,23 +2,47 @@ def parseInput():
     with open("input", 'r') as input_file:
         return input_file.readlines()
 
-def algorithm():
-    pass
+def buildGraph(data):
+    graph = {}
+    for d in data:
+        planets = d.split(')')
+        graph[planets[1].strip()] = planets[0].strip()
+    return graph
 
-def one():
-    input_list = parseInput()
-    result = 0
-    for i in input_list:
-        algorithm()
-    print(1, result)
+def calculateOrbits(graph):     
+    orbits = 0
+    for planet, node in graph.items():
+        orbits += calculateIndirectOrbits(node, graph)
+    return orbits
 
-def two():
-    input_list = parseInput()
-    result = 0
-    for i in input_list:
-        algorithm()
+def calculateIndirectOrbits(node, graph):
+    orbits = 1
+    if graph.get(node):
+        orbits += calculateIndirectOrbits(graph.get(node), graph)
+    return orbits
 
-    print(2, result)
+def findDistance(graph):
+    santa = graph.get('SAN')
+    you = graph.get('YOU')
+    santa_parents = []
+    you_parents = []
+    while santa:
+        santa_parents.append(santa)
+        santa = graph.get(santa)
+    while you:
+        you_parents.append(you)
+        you = graph.get(you)
+    diff = list(set(santa_parents)- set(you_parents))
+    diff.extend(list(set(you_parents)- set(santa_parents)))
+    return len(diff)
+               
+def solution():
+    data = parseInput()
+    graph = buildGraph(data)
+    orbits = calculateOrbits(graph)
+    distance = findDistance(graph)
+    print(1, orbits)
+    print(2, distance)
+
 if __name__ == "__main__":
-    one()
-    two()
+    solution()
